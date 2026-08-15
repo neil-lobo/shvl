528883ca65f8d1d89300c80e0fbdc214c592fd38
neil-lobo <neil_edward_lobo@hotmail.com>
2026-08-15 12:53:22 -0400

    nix: fix pname

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
   pkgs ? import <nixpkgs> { },
 }:
 pkgs.rustPlatform.buildRustPackage rec {
-  pname = "nixos-add-package";
+  pname = "shvl";
   version = "0.1.0";
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
